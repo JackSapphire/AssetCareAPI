19bf4c6a07174e6a6570d10f0fd538f5f5adf670
Álvaro Secundino <alvaro1043@gmail.com>
2026-01-04 20:42:45 -0300

    Inclusão de ID para Manutenções

--- AssetCare API/Models/Manutencao.cs
+++ AssetCare API/Models/Manutencao.cs
@@ -2,9 +2,10 @@
 
 public class Manutencao
 {
-    public int Id { get; set; }
-    public int EquipmentoId { get; set; }
-    public int TecnicoId { get; set; }
+    public int MaquinaId { get; set; }
+    public virtual Maquina? Maquina { get; set; }
+    public string TecnicoId { get; set; } = string.Empty;
+    public virtual Tecnico? Tecnico { get; set; }
     public DateTime DataAgendada { get; set; }
     public DateTime? DataExecucao { get; set; }
     public string Descricao { get; set; } = string.Empty;
